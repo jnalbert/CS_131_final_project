11a40d6d10be50952b025eba46f4357fbc97abd2
Andrew <andytgarcia27@gmail.com>
2025-02-23 16:10:27 -0800

    Db and hand tracking

---
+++ db/handle_db.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('password_manager.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        );
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+
+def insert_user(username, password):
+    conn = sqlite3.connect('password_manager.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO users (username, password) VALUES (?, ?)
+    ''', (username, password))
+
+    conn.commit()
+    conn.close()
+
+
+def get_user(username):
+    conn = sqlite3.connect('password_manager.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT * FROM users WHERE username = ?
+    ''', (username,))
+    user = cursor.fetchone()
+
+    conn.close()
+    return user
+
+def retreieve_password(username):
+    conn = sqlite3.connect('password_manager.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT password FROM users WHERE username = ?
+    ''', (username,))
+    password = cursor.fetchone()
+
+    conn.close()
+    
+    if password is None:
+        return None
+    return password
+    
+
+def main():
+    init_db()
+
+    #example code
+    insert_user('admin', 'admin')
+    print(get_user('admin'))
+    print(retreieve_password('admin'))
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
