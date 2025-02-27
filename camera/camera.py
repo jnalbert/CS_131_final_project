e7e1e6191dec096e8e6eac1ef85e40da59479c24
Justin Albert <jnalbert879@gmail.com>
2025-02-27 13:41:12 -0800

    abotger

--- camera/camera.py
+++ camera/camera.py
@@ -1,56 +1,56 @@
+import sys
+
 import cv2
 
-# https://stackoverflow.com/questions/64272731/open-cv-shows-green-screen-on-jetson-nano
+window_title = "USB Camera"
 
+# ASSIGN CAMERA ADRESS to DEVICE HERE!
+pipeline = " ! ".join(["v4l2src device=/dev/video0",
+                       "video/x-raw, width=640, height=480, framerate=30/1",
+                       "videoconvert",
+                       "video/x-raw, format=(string)BGR",
+                       "appsink"
+                       ])
 
-
-def gstreamer_pipeline(
-    capture_width=1920,
-    capture_height=1080,
-    display_width=1920,
-    display_height=1080,
-    framerate=21,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc sensor-id=0 ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
+# Sample pipeline for H.264 video, tested on Logitech C920
+h264_pipeline = " ! ".join(["v4l2src device=/dev/video0",
+                            "video/x-h264, width=1280, height=720, framerate=30/1, format=H264",
+                            "avdec_h264",
+                            "videoconvert",
+                            "video/x-raw, format=(string)BGR",
+                            "appsink sync=false"
+                            ])
 
 def show_camera():
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    if cap.isOpened():
-        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-        # Window
-        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
-            ret_val, img = cap.read()
-            cv2.imshow("CSI Camera", img)
-            # This also acts as
-            keyCode = cv2.waitKey(30) & 0xFF
-            # Stop the program on the ESC key
-            if keyCode == 27:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
+
+    # Full list of Video Capture APIs (video backends): https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html
+    # For webcams, we use V4L2
+    video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+
+    if video_capture.isOpened():
+        try:
+            window_handle = cv2.namedWindow(
+                window_title, cv2.WINDOW_AUTOSIZE)
+            # Window
+            while True:
+                ret_val, frame = video_capture.read()
+                # Check to see if the user closed the window
+                # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+                # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+                if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(window_title, frame)
+                else:
+                    break
+                keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
     else:
-        print("Unable to open camera")
+        print("Error: Unable to open camera")
 
 
 if __name__ == "__main__":
